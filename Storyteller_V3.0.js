749fd97b1c86d5b0e98d57295a481b608a3f9ecb
Eureka <31796549+zhenshiluosuo@users.noreply.github.com>
2020-02-22 22:48:32 +0800

    更新了多句转轮模式，可循环发送多句 Greasyfork请搜索独轮车-说书人自动弹幕发射器

--- Storyteller_V3.0.js
+++ Storyteller_V3.0.js
@@ -3,8 +3,8 @@ let div2 = document.createElement('div');//控制台
 let css1 = 'background: #1A59B7;color:#ffffff;overflow: hidden;z-index: 998;position: fixed;padding:5px;text-align:center;width: 75px;height: 22px;border-bottom-left-radius: 4px;border-bottom-right-radius: 4px;border-top-left-radius: 4px;border-top-right-radius: 4px;right: 10px;top: 30%;'
 let css2 = 'background: #E5E4E4;color:#ffffff;overflow: hidden;z-index: 999;position: fixed;padding:5px;text-align:center;width: 150px;height: 275px;border-color: #FFFFFF;border: 3px;border-bottom-left-radius: 4px;border-bottom-right-radius: 4px;border-top-left-radius: 4px;border-top-right-radius: 4px;right: 10px;top: 30%;display: none;';
 let max_danmu_long = 43;//弹幕字数限制
-const min_danmu_long = 22;//最小弹幕长度
-const error_danmu_long = 33;//防止无法断句弹幕长度
+const min_danmu_long = 20;//最小弹幕长度
+const error_danmu_long = 30;//防止无法断句弹幕长度
 let cycle_time;//弹幕周期，单位毫秒 建议设定至6000毫秒以上 过低有系统屏蔽风险
 let story;//textarea内容
 let story_arr = [];//story分段
@@ -20,7 +20,7 @@ function init() {
     div1.style.cssText = css1;
     div2.style.cssText = css2;
     div1.innerHTML = '独轮车控制台';
-    div2.innerHTML = '<select id="DuLunCheSelect"><option value="0">单句模式</option><option value="1">说书模式</option></select><textarea id="DuLunCheText" rows="10" cols="20" placeholder="输入内容"></textarea><div  style="margin: 0 auto;"><input type="text" placeholder="间隔时间(ms) 建议六千以上" id="DuLunCheTime"/><button id="DuLunCheBtn" style="background-color: #FFFFFF;">出动！</button><br><button id="DuLunCheYincang" style="background-color: #FFFFFF;">隐藏控制台</button></div>';
+    div2.innerHTML = '<select id="DuLunCheSelect"><option value="0">单句模式</option><option value="1">说书模式</option><option value="2">多句转轮</option></select><textarea id="DuLunCheText" rows="10" cols="20" placeholder="输入内容"></textarea><div  style="margin: 0 auto;"><input type="text" placeholder="间隔时间(ms) 建议六千以上" id="DuLunCheTime"/><button id="DuLunCheBtn" style="background-color: #FFFFFF;">出动！</button><br><button id="DuLunCheYincang" style="background-color: #FFFFFF;">隐藏控制台</button></div>';
     div1.onclick = () => {div2.style.setProperty('display','block');}
     document.body.appendChild(div1);
     document.body.appendChild(div2);
@@ -29,7 +29,7 @@ function init() {
         if(document.getElementById('DuLunCheBtn').innerText === '出动！') run();
         else finish();
     }
-    alert('欢迎使用说书人自动弹幕发射装置V3.0，本插件由斗鱼用户重载操作符和祖冲之丶丶基于祖冲之丶丶V1.5版制作，项目地址：https://github.com/zhenshiluosuo/Storyteller-AutoBarrageForDouyuTV/ 为了自己的账号和他人观看体验，建议发言间隔调至8000以上，喜欢的好兄弟打个星星吧~求求了！！！');
+    alert('欢迎使用说书人自动弹幕发射装置V3.0，本插件由斗鱼用户重载操作符和祖冲之丶丶基于祖冲之丶丶V1.5版制作，项目地址：https://github.com/zhenshiluosuo/Storyteller-AutoBarrageForDouyuTV/ 多句转轮模式每句之间请用回车分隔，为了自己的账号和他人观看体验，建议发言间隔调至8000以上，喜欢的好兄弟打个星星吧~求求了！！！');
 }
 
 function run() {
@@ -39,16 +39,17 @@ function run() {
     story = document.getElementById('DuLunCheText').value;
     cycle_time = parseInt(document.getElementById('DuLunCheTime').value);
     if(!story.length || !cycle_time){
-        alert('请勿空置运行！')
+        alert('请勿空置运行！');
         finish();
         return;
     }else if(cycle_time <= 2999) {
-        alert('请珍惜账号 加大发言间隔！')
+        alert('请珍惜账号 加大发言间隔！');
         finish();
-        document.getElementById('DuLunCheTime').value = '9999'
+        document.getElementById('DuLunCheTime').value = '9999';
         return;
     }
-    if(document.getElementById('DuLunCheSelect').value === '0') {
+    let _value = document.getElementById('DuLunCheSelect').value;
+    if(_value === '0') {
         if (story.length > max_danmu_long){
             story = story.slice(0,max_danmu_long + 1);
         }
@@ -58,8 +59,11 @@ function run() {
                 btn.click();
             }
         }, cycle_time);
-    }else {
-        get_better_sentence();
+    }else{
+        if(_value === '1')
+            get_better_sentence();
+        else
+            multiple();
         let len = story_arr.length;
         index = 0;
         interval = setInterval(() => {
@@ -107,6 +111,25 @@ function get_better_sentence() {
             continue;
         }else {
             str += story.charAt(i);
+        }
+    }
+}
+
+function multiple() {
+    let len = story.length;
+    let str = '';
+    let flag = true;
+    for (let i = 0; i < len; i++){
+        if((story.charAt(i) === '\n') && str.length){
+            story_arr.push(str);
+            str = '';
+        }else if(str.length > 43){
+            continue;
+        }else{
+            str += story.charAt(i);
+            if(i === len - 1){
+                story_arr.push(str);
+            }
         }
     }
 }
--- Storyteller_VM.js
+++ Storyteller_VM.js
@@ -5,7 +5,7 @@
 // @author        闪光魔法师
 // @description   适配斗鱼直播平台的自动弹幕发射器 抽象独轮车 说书人 Github:https://github.com/zhenshiluosuo/Storyteller-AutoBarrageForDouyuTV
 // @match         *://www.douyu.com/*
-// @version       0.0.1
+// @version       0.0.2
 // @grant         none
 // @updateURL     https://github.com/zhenshiluosuo/Storyteller-AutoBarrageForDouyuTV/blob/master/Storyteller_VM.js
 // ==/UserScript==
@@ -18,8 +18,8 @@
     let css1 = 'background: #1A59B7;color:#ffffff;overflow: hidden;z-index: 998;position: fixed;padding:5px;text-align:center;width: 75px;height: 22px;border-bottom-left-radius: 4px;border-bottom-right-radius: 4px;border-top-left-radius: 4px;border-top-right-radius: 4px;right: 10px;top: 30%;'
     let css2 = 'background: #E5E4E4;color:#ffffff;overflow: hidden;z-index: 999;position: fixed;padding:5px;text-align:center;width: 150px;height: 275px;border-color: #FFFFFF;border: 3px;border-bottom-left-radius: 4px;border-bottom-right-radius: 4px;border-top-left-radius: 4px;border-top-right-radius: 4px;right: 10px;top: 30%;display: none;';
     let max_danmu_long = 43;//弹幕字数限制
-    const min_danmu_long = 22;//最小弹幕长度
-    const error_danmu_long = 33;//防止无法断句弹幕长度
+    const min_danmu_long = 20;//最小弹幕长度
+    const error_danmu_long = 30;//防止无法断句弹幕长度
     let cycle_time;//弹幕周期，单位毫秒 建议设定至6000毫秒以上 过低有系统屏蔽风险
     let story;//textarea内容
     let story_arr = [];//story分段
@@ -35,19 +35,19 @@
         div1.style.cssText = css1;
         div2.style.cssText = css2;
         div1.innerHTML = '独轮车控制台';
-        div2.innerHTML = '<select id="DuLunCheSelect"><option value="0">单句模式</option><option value="1">说书模式</option></select><textarea id="DuLunCheText" rows="10" cols="20" placeholder="输入要发射的内容 斗鱼限制单次最多43字"></textarea><div  style="margin: 0 auto;"><input type="text" placeholder="间隔时间(ms) 最少3000" id="DuLunCheTime"/><button id="DuLunCheBtn" style="background-color: #FFFFFF;">出动！</button><br><button id="DuLunCheYincang" style="background-color: #FFFFFF;">隐藏控制台</button></div>';
+        div2.innerHTML = '<select id="DuLunCheSelect"><option value="0">单句模式</option><option value="1">说书模式</option><option value="2">多句转轮</option></select><textarea id="DuLunCheText" rows="10" cols="20" placeholder="输入要发射的内容 斗鱼限制单次最多43字"></textarea><div  style="margin: 0 auto;"><input type="text" placeholder="间隔时间(ms) 最少3000" id="DuLunCheTime"/><button id="DuLunCheBtn" style="background-color: #FFFFFF;">出动！</button><br><button id="DuLunCheYincang" style="background-color: #FFFFFF;">隐藏控制台</button></div>';
         div1.onclick = () => {
             div2.style.setProperty('display','block');
             if(!tip){
                 tip = true;
-                alert('欢迎使用说书人自动弹幕发射装置V3.0，本插件由斗鱼用户重载操作符和祖冲之丶丶基于祖冲之丶丶版制作，项目地址：https://github.com/zhenshiluosuo/Storyteller-AutoBarrageForDouyuTV/ 为了自己的账号和他人观看体验，建议发言间隔调至8000以上，喜欢的好兄弟打个星星吧~求求了！！！');
+                alert('欢迎使用说书人自动弹幕发射装置V3.0，本插件由斗鱼用户重载操作符和祖冲之丶丶基于祖冲之丶丶版制作，项目地址：https://github.com/zhenshiluosuo/Storyteller-AutoBarrageForDouyuTV/ 多句转轮模式每句之间请用回车分隔，斗鱼字数限制43，为了自己的账号和他人观看体验，建议发言间隔调至8000以上，喜欢的好兄弟打个星星吧~求求了！！！');
             }
-        }
+        };
         document.body.appendChild(div1);
         document.body.appendChild(div2);
         document.getElementById('DuLunCheYincang').onclick = () => {
             div2.style.setProperty('display','none');
-        }
+        };
         document.getElementById('DuLunCheBtn').onclick = () => {
             if(document.getElementById('DuLunCheBtn').innerText === '出动！') run();
             else finish();
@@ -61,16 +61,17 @@
         story = document.getElementById('DuLunCheText').value;
         cycle_time = parseInt(document.getElementById('DuLunCheTime').value);
         if(!story.length || !cycle_time){
-            alert('请勿空置运行！')
+            alert('请勿空置运行！');
             finish();
             return;
         }else if(cycle_time <= 2999) {
-            alert('请珍惜账号 加大发言间隔！')
+            alert('请珍惜账号 加大发言间隔！');
             finish();
-            document.getElementById('DuLunCheTime').value = '9999'
+            document.getElementById('DuLunCheTime').value = '9999';
             return;
         }
-        if(document.getElementById('DuLunCheSelect').value === '0') {
+        let _value = document.getElementById('DuLunCheSelect').value;
+        if(_value === '0') {
             if (story.length > max_danmu_long){
                 story = story.slice(0,max_danmu_long + 1);
             }
@@ -81,7 +82,10 @@
                 }
             }, cycle_time);
         }else {
-            get_better_sentence();
+            if(_value === '1')
+                get_better_sentence();
+            else
+                multiple();
             let len = story_arr.length;
             index = 0;
             interval = setInterval(() => {
@@ -129,6 +133,25 @@
                 continue;
             }else {
                 str += story.charAt(i);
+            }
+        }
+    }
+
+    function multiple() {
+        let len = story.length;
+        let str = '';
+        let flag = true;
+        for (let i = 0; i < len; i++){
+            if((story.charAt(i) === '\n') && str.length){
+                story_arr.push(str);
+                str = '';
+            }else if(str.length > 43){
+                continue;
+            }else{
+                str += story.charAt(i);
+                if(i === len - 1){
+                    story_arr.push(str);
+                }
             }
         }
     }
